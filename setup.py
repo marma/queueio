3e2f3f41e6baf2ed88f561999eb04d201399f084
Martin Malmsten <martin.malmsten@kb.se>
2020-07-19 16:32:13 +0200

    First

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='queueio', version='0.0.1', description='', author='Martin Malmsten', author_email='martin@martinmalmsten.net', packages=[ 'queueio' ])
+
